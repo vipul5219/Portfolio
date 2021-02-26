a359049d608a9d18939b1c3159c8c7bd484eedc3
vipul5219 <chaudharivipul16@gmail.com>
2021-02-25 23:44:38 -0500

    Implemented Menus and Landing Page

--- app/src/main/java/com/example/portfolio/Personal_data.java
+++ app/src/main/java/com/example/portfolio/Personal_data.java
@@ -9,31 +9,31 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class Personal_data extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personal_data);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        inflater.inflate(R.menu.personal_menu,menu);
         return true;
 
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.personal_data:
-                startActivity(new Intent(MainActivity.this, Personal_data.class));
+            case R.id.resume:
+                startActivity(new Intent(Personal_data.this, Resume.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
 
         }
     }
-
 }
